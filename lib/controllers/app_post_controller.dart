2bdf633a5a0b60efd40b604cc680225ee96998ff
Firkat <57084614+firkatdavletov@users.noreply.github.com>
2022-12-05 21:05:21 +0500

    Add create post method

--- lib/controllers/app_post_controller.dart
+++ lib/controllers/app_post_controller.dart
@@ -1,12 +1,39 @@
+import 'dart:io';
+
 import 'package:data/utils/app_response.dart';
 import 'package:conduit/conduit.dart';
 import 'package:data/models/post.dart';
 import 'package:data/models/author.dart';
+import 'package:data/utils/app_utils.dart';
 
 class AppPostController extends ResourceController {
   final ManagedContext managedContext;
 
   AppPostController(this.managedContext);
+
+  @Operation.post()
+  Future<Response> createPosts(
+      @Bind.header(HttpHeaders.authorizationHeader) String header,
+      @Bind.body() Post post) async {
+    try {
+      final id = AppUtils.getIdFromHeader(header);
+      final author = await managedContext.fetchObjectWithID<Author>(id);
+
+      if (author == null) {
+        final qCreateAuthor = Query<Author>(managedContext)..values.id = id;
+        await qCreateAuthor.insert();
+      }
+
+      final qCreatePost = Query<Post>(managedContext)
+        ..values.author?.id = id
+        ..values.content = post.content;
+      await qCreatePost.insert();
+
+      return AppResponse.ok(message: "Успешное создание поста");
+    } catch (error) {
+      return AppResponse.serverError(error, message: "Ошибка создания поста");
+    }
+  }
 
   @Operation.get()
   Future<Response> getPosts() async {
