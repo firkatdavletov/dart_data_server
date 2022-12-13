5e0e43991462b9ddba35543e7f751f046f64adaf
Firkat <57084614+firkatdavletov@users.noreply.github.com>
2022-12-13 20:59:01 +0500

    Modifies post model

--- lib/controllers/app_post_controller.dart
+++ lib/controllers/app_post_controller.dart
@@ -15,6 +15,13 @@ class AppPostController extends ResourceController {
   Future<Response> createPosts(
       @Bind.header(HttpHeaders.authorizationHeader) String header,
       @Bind.body() Post post) async {
+    if (post.content == null ||
+        post.content?.isEmpty == true ||
+        post.name == null ||
+        post.name?.isEmpty == true) {
+      return AppResponse.badRequest(message: "Поля name и content обязательны");
+    }
+
     try {
       final id = AppUtils.getIdFromHeader(header);
       final author = await managedContext.fetchObjectWithID<Author>(id);
@@ -24,8 +31,12 @@ class AppPostController extends ResourceController {
         await qCreateAuthor.insert();
       }
 
+      final size = post.content?.length ?? 0;
+
       final qCreatePost = Query<Post>(managedContext)
         ..values.author?.id = id
+        ..values.name = post.name
+        ..values.preContent = post.content?.substring(0, size <= 20 ? size : 20)
         ..values.content = post.content;
       await qCreatePost.insert();
 
@@ -41,17 +52,16 @@ class AppPostController extends ResourceController {
       @Bind.path("id") int id) async {
     try {
       final currentAuthorId = AppUtils.getIdFromHeader(header);
-      final post = await managedContext.fetchObjectWithID<Post>(id);
+      final qGetPost = Query<Post>(managedContext)
+        ..where((x) => x.id).equalTo(id)
+        ..where((x) => x.author?.id).equalTo(currentAuthorId)
+        ..returningProperties((x) => [x.content, x.id, x.name]);
+
+      final post = await qGetPost.fetchOne();
 
       if (post == null) {
         return AppResponse.badRequest(message: "Пост не найден");
       }
-
-      if (post.author?.id != currentAuthorId) {
-        return AppResponse.badRequest(message: "Нет доступа к посту");
-      }
-
-      post.backing.removeProperty("author");
 
       return AppResponse.ok(
           body: post.backing.contents, message: "Успешное получение поста");
